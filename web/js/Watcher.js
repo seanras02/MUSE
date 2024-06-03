329ee9824d73c135970d2c46738ee8a556648921
JayJairam <jmojairam@gmail.com>
2024-06-03 11:50:56 +0200

    view model page
    heb de view model page gedeeltelijk af je kan nu elke glb(model) zetten en bekijken

---
+++ web/js/Watcher.js
@@ -0,0 +1,79 @@
+
+var scene = new THREE.Scene();
+
+
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+var loader = new THREE.GLTFLoader();
+loader.load('models/Watcher.glb', function (gltf) {
+    model = gltf.scene;
+    scene.add(model);
+});
+
+
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
+
+// Interactive controls
+var isDragging = false;
+var previousMousePosition = {
+    x: 0,
+    y: 0
+};
+
+document.addEventListener('mousedown', function(event) {
+    isDragging = true;
+});
+
+document.addEventListener('mousemove', function(event) {
+    var deltaMove = {
+        x: event.clientX - previousMousePosition.x,
+        y: event.clientY - previousMousePosition.y
+    };
+
+    if (isDragging) {
+        var deltaRotationQuaternion = new THREE.Quaternion().setFromEuler(
+            new THREE.Euler(
+                toRadians(deltaMove.y * 1),
+                toRadians(deltaMove.x * 1),
+                0,
+                'XYZ'
+            )
+        );
+
+        model.quaternion.multiplyQuaternions(deltaRotationQuaternion, model.quaternion);
+    }
+
+    previousMousePosition = {
+        x: event.clientX,
+        y: event.clientY
+    };
+});
+
+document.addEventListener('mouseup', function(event) {
+    isDragging = false;
+});
+
+function toRadians(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
+
+function resetPage() {
+  location.reload();
+}
