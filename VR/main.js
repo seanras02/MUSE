7d41cc313a507b89d9c9b41d7da468400e0a98db
Sean <34967@ma-web.nl>
2024-06-17 14:42:33 +0200

    arduino done vr progress

---
+++ VR/main.js
@@ -0,0 +1,21 @@
+AFRAME.registerComponent('multiple-characters', {
+    init: function () {
+        const sceneEl = this.el;
+        const characterCount = 10;
+        const initialZ = -30;
+        const maxZ = -50;
+        const minX = -20;
+        const maxX = 20;
+
+        for (let i = 0; i < characterCount; i++) {
+            const z = initialZ - Math.random() * (initialZ - maxZ);
+            const x = minX + Math.random() * (maxX - minX);
+            const entity = document.createElement('a-entity');
+            entity.setAttribute('gltf-model', '#characterModel');
+            entity.setAttribute('position', { x: x, y: -0.10, z: z });
+            entity.setAttribute('animation-mixer', '');
+            entity.setAttribute('animation', `property: position; to: ${x} -0.10 1.5; dur: 2500; easing: linear; loop: true`);
+            sceneEl.appendChild(entity);
+        }
+    }
+});
